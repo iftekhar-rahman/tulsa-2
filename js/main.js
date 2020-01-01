7ee3d5f99bc649f498074b82d9d896fce5f5e1dc
Iftekhar Rahman <iftekharmail1@gmail.com>
2020-01-01 04:51:14 -0800

    sdas

--- js/main.js
+++ js/main.js
@@ -103,41 +103,41 @@ jQuery(document).ready(function($){
 
         }
     });
+    // featured-carousel
+    $(".featured-wrap").owlCarousel({
+        items: 4,
+        loop: true,
+        nav: true,
+        dots: false,
+        autoplay: false,
+        margin: 15,
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        responsive: {
+            0: {
+                items: 1,
+                margin: 0,
+                nav: false
+            },
+            481: {
+                items: 2,
+                margin: 15,
+                nav: false
+            },
+            720: {
+                items: 3,
+                margin: 15,
+                nav: false
+            },
+            960: {
+                items: 3,
+                margin: 25,
+            },
+            1140: {
+                items: 4
+            }
 
-    // $(".feature-carousel-wrap").owlCarousel({
-    //     items: 4,
-    //     loop: true,
-    //     nav: true,
-    //     dots: true,
-    //     autoplay: false,
-    //     margin: 15,
-    //     navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
-    //     responsive: {
-    //         0: {
-    //             items: 1,
-    //             margin: 0,
-    //             nav: false
-    //         },
-    //         481: {
-    //             items: 2,
-    //             margin: 15,
-    //             nav: false
-    //         },
-    //         720: {
-    //             items: 3,
-    //             margin: 15,
-    //             nav: false
-    //         },
-    //         960: {
-    //             items: 3,
-    //             margin: 25,
-    //         },
-    //         1140: {
-    //             items: 4
-    //         }
-
-    //     }
-    // });
+        }
+    });
 
 
 
